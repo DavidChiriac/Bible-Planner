c2a856e2300036f4452a035fd4760c14014d3851
CrcDavid <206734280+t-David-Chiriac_avoltagh@users.noreply.github.com>
2026-02-13 22:15:41 +0200

    change the env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyB_QHKdpRVW8QJFvs_3SnSokOZ2noHoasU",
-    authDomain: "bible-planner-e5ad3.firebaseapp.com",
-    databaseURL: "https://bible-planner-e5ad3-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "bible-planner-e5ad3",
-    storageBucket: "bible-planner-e5ad3.firebasestorage.app",
-    messagingSenderId: "1080524153981",
-    appId: "1:1080524153981:web:90cf8df1e6b1e7f956bd14"
+    apiKey: "AIzaSyBRKKxmJ4Z45_H9G_4Uja94Q-oTtqNCUmQ",
+    authDomain: "bible-planner-prod.firebaseapp.com",
+    databaseURL: "https://bible-planner-prod-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "bible-planner-prod",
+    storageBucket: "bible-planner-prod.firebasestorage.app",
+    messagingSenderId: "820329179054",
+    appId: "1:820329179054:web:e2b32700e0019b43e54a3f",
+    measurementId: "G-RB5VH8G8QC"
   }
 };
