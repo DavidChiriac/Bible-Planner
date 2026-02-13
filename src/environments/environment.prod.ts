eaa2239a2c99210d898743cfb42e9c4a6cd8d70f
CrcDavid <206734280+t-David-Chiriac_avoltagh@users.noreply.github.com>
2026-02-13 22:09:43 +0200

    add environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    databaseURL: ""
+    apiKey: "AIzaSyB_QHKdpRVW8QJFvs_3SnSokOZ2noHoasU",
+    authDomain: "bible-planner-e5ad3.firebaseapp.com",
+    databaseURL: "https://bible-planner-e5ad3-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "bible-planner-e5ad3",
+    storageBucket: "bible-planner-e5ad3.firebasestorage.app",
+    messagingSenderId: "1080524153981",
+    appId: "1:1080524153981:web:90cf8df1e6b1e7f956bd14"
   }
 };
