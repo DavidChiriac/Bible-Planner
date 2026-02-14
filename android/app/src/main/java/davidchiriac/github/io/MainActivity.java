b8e9b08ce775f1e634dea9ce3f36fb51503ab366
CrcDavid <206734280+t-David-Chiriac_avoltagh@users.noreply.github.com>
2026-02-14 14:16:00 +0200

    Add capacitor

---
+++ android/app/src/main/java/davidchiriac/github/io/MainActivity.java
@@ -0,0 +1,5 @@
+package davidchiriac.github.io;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'davidchiriac.github.io',
+  appName: 'Plan citire Biblie',
+  webDir: 'dist/bible-planner/browser',
+};
+
+export default config;
